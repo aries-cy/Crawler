452a76356d1015ff4d46944b443e52a69bcdb33e
cao_yang <cao_yang@icekredit.com>
2019-04-17 18:33:25 +0800

    将爬下来的数据封装成对象

--- example/csdn_title.py
+++ example/csdn_title.py
@@ -1,7 +1,17 @@
 import re
 import urllib.request
 
+from example.Book import Book
+
 data = urllib.request.urlopen("https://gitchat.csdn.net/?utm_source=csdn_toolbar").read().decode("utf-8")
-pat = "<h3 class=\"item_title_m\">(.*?)</h3>"
-rst = re.compile(pat).findall(data)
-print(rst)
+#patTitle = "<h3 class=\"item_title_m\">(.*?)</h3>"
+result = re.findall(r'<h3 class="item_title">(.*?)</h3>.*?<p class="item_name">(.*?)</p>',data,re.S|re.M)
+print(result)
+#rst = re.compile(patTitle).findall(data)
+books = []
+for title,name in result:
+    book = Book(title,name)
+    books.append(book)
+
+for msg in books:
+    print(Book.display_author(msg))
