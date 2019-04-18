c130286d84bc460630f74fc26c00d3179fb238b0
cao_yang <cao_yang@icekredit.com>
2019-04-18 14:54:44 +0800

    将爬取的数据存入数据库

--- example/csdn_title.py
+++ example/csdn_title.py
@@ -1,17 +1,25 @@
 import re
 import urllib.request
-
-from example.Book import Book
+import mysql.connector
 
 data = urllib.request.urlopen("https://gitchat.csdn.net/?utm_source=csdn_toolbar").read().decode("utf-8")
-#patTitle = "<h3 class=\"item_title_m\">(.*?)</h3>"
 result = re.findall(r'<h3 class="item_title">(.*?)</h3>.*?<p class="item_name">(.*?)</p>',data,re.S|re.M)
-print(result)
-#rst = re.compile(patTitle).findall(data)
 books = []
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="root",
+  database="cy"
+)
+cursor = db.cursor()
+sql = "insert into book (title,author) values (%s,%s)"
 for title,name in result:
-    book = Book(title,name)
-    books.append(book)
-
-for msg in books:
-    print(Book.display_author(msg))
+    val = (title,name)
+    books.append(val)
+cursor.executemany(sql,books)
+db.commit()
+sql = "select * from  book"
+cursor.execute(sql)
+rst = cursor.fetchall()
+for x in rst:
+    print(x)
